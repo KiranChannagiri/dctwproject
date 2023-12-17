f5853d2b6a387525358a0e06cca2e699dfe6cda8
Kiran K Channagiri <kirankc1704@gmail.com>
2023-12-17 18:33:49 +0530

    final uplaod discord clone

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Whitney:["Whitney"],
+        Ginto:["Ginto"],
+        ggSans:["ggSans"],
+        new:["ABC Ginto Nord","Noto Sans","Helvetica Neue","Helvetica","Arial","sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
